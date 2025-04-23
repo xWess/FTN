2e999ab26b5b4447a97562e55166acaa8f48faea
Wess <dridi.wissem@etudiant-fst.utm.tn>
2025-04-24 00:47:56 +0100

    Initial commit: Project setup with Angular frontend and Spring Boot backend

---
+++ src/test/java/ftn/alinfo/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.alinfo.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
